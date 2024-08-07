d7e682da8c9c4ffcc07884a4c375ce5eb3c641fd
webreflection <andrea.giammarchi@gmail.com>
2024-08-07 16:02:07 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,87 @@
+# just-mime
+
+A simple way to retrieve a mime-type from an extension.
+
+## Usage
+
+```sh
+# prints out `text/javascript`
+npx just-mime --for js
+
+# prints out `application/javascript`
+npx just-mime --db apache --for js
+
+# prints out an ECMAScript module to
+# eventually store and use somewhere
+npx just-mime --db apache > mime.js
+
+# prints out an ESM with only some
+# extension recognized
+npx just-mime --db=apache,iana --include=png,html,css,jpg,js,svg
+
+# prints out an ESM with all possible results
+# except those mime types that are not desired
+# (include is for extensions, exclude is for mime types)
+npx just-mime --exclude ^vnd.,^x-,others
+# exclude supports ^ and $ for prefix/suffix cases
+
+# it shows an informative help message
+npx just-mime --help
+```
+
+### Output Example
+
+```js
+const APPLICATION = "application";
+const IMAGE = "image";
+const TEXT = "text";
+
+const types = {
+  [APPLICATION]: ["javascript"],
+  [IMAGE]: ["jpeg", "png", "svg+xml"],
+  [TEXT]: ["css", "html"]
+};
+
+const extensions = {
+  "js": [APPLICATION, 0],
+  "jpeg": [IMAGE, 0],
+  "jpg": [IMAGE, 0],
+  "jpe": [IMAGE, 0],
+  "png": [IMAGE, 1],
+  "svg": [IMAGE, 2],
+  "svgz": [IMAGE, 2],
+  "css": [TEXT, 0],
+  "html": [TEXT, 1],
+  "htm": [TEXT, 1],
+  "shtml": [TEXT, 1],
+  "mjs": [TEXT, 2]
+};
+
+const noDot = type => type.startsWith('.') ? type.slice(1) : type;
+
+export default new Proxy(
+  extensions,
+  {
+    has: ($, type) => $.hasOwnProperty(noDot(type)),
+    get: ($, type) => {
+      const value = $[noDot(type)];
+      return value && `${value[0]}/${types[value[0]][value[1]]}`;
+    },
+  }
+);
+```
+
+The created module is highly optimized to avoid repetition, it plays extremely well with minifiers, and it's really fast to return results.
+
+```js
+import mime from 'just-mime.js';
+
+console.log('js' in mime);
+// true
+
+console.log(mime.js);
+// "application/javascript"
+
+console.log(mime.shenanigans);
+// undefined
+```
---
+++ index.js
@@ -0,0 +1,197 @@
+#!/usr/bin/env node
+import db from 'mime-db';
+
+const { create, entries, is } = Object;
+const { stringify } = JSON;
+
+const types = create(null);
+const values = create(null);
+const constants = new Map;
+const include = new Set;
+const sources = new Set;
+const valid = new Set;
+const exclude = [];
+
+const findKey = $ => {
+  for (const [key, value] of constants) {
+    if (is(value, $)) return key;
+  }
+};
+
+const noDot = type => type.startsWith('.') ? type.slice(1) : type;
+
+const args = [];
+
+for (const arg of process.argv.slice(2)) {
+  if (arg.includes('=')) {
+    const [name, ...value] = arg.split('=');
+    args.push(name, value.join('='));
+  }
+  else
+    args.push(arg);
+}
+
+let oneOff = '';
+
+for (let i = 0; i < args.length; i++) {
+  switch (args[i]) {
+    case '--db': {
+      for (const source of (args.at(++i) || '').split(','))
+        sources.add(source);
+      break;
+    }
+    case '--exclude': {
+      const exts = (args.at(++i) || '').split(',');
+      for (let value of exts) {
+        if (value.startsWith('^')) {
+          value = value.slice(1);
+          exclude.push(s => s.startsWith(value));
+        }
+        else if (value.endsWith('$')) {
+          value = value.slice(0, -1);
+          exclude.push(s => s.endsWith(value));
+        }
+        else
+          exclude.push(s => s.includes(value));
+      }
+      break;
+    }
+    case '--include': {
+      const exts = (args.at(++i) || '').split(',').map(noDot);
+      if (exts.length) {
+        for (const ext of exts)
+          include.add(ext);
+      }
+      else {
+        console.error(`Unknown extension in ${exts.join(',')} list`);
+        process.exit(1);
+      }
+      break;
+    }
+    case '--for': {
+      oneOff = noDot(args.at(++i) || '');
+      if (!oneOff) {
+        console.error(`Undefined --for extension`);
+        process.exit(1);
+      }
+      break;
+    }
+    case '--help': {
+      console.log(`
+\x1b[1mjust-mime\x1b[0m [options]
+
+\x1b[2m# convert an extension to its mime-type or
+# produce an optimized ESM file to do the same\x1b[0m
+
+    --help                    \x1b[2m# this message\x1b[0m
+    --db apache,iana,nginx    \x1b[2m# the optional db source to consider\x1b[0m
+    --for ext                 \x1b[2m# convert an extension to its mime-type\x1b[0m
+    --include png,svg,txt     \x1b[2m# extensions (only) to include\x1b[0m
+    --exclude ^vnd.,^x-,woff  \x1b[2m# extensions or mime to exclude\x1b[0m
+
+\x1b[2m# (c) Andrea Giammarchi - MIT\x1b[0m
+\x1b[2m# https://github.com/WebReflection/just-mime\x1b[0m
+`);
+      process.exit(0);
+      break;
+    }
+  }
+}
+
+for (const [key, details] of entries(db)) {
+  const { extensions, source } = details;
+  if (source) valid.add(source);
+  if (!extensions || (sources.size && !sources.has(source))) continue;
+  if (include.size) {
+    let consider = false;
+    for (const value of extensions) {
+      if (include.has(value)) {
+        consider = true;
+        break;
+      }
+    }
+    if (!consider) continue;
+  }
+  const [prefix, ...rest] = key.split('/');
+  const pre = prefix.toUpperCase().replace(/-/g, '_');
+  if (!constants.has(pre))
+    constants.set(pre, prefix);
+  if (!(prefix in types))
+    types[prefix] = [];
+  const mime = rest.join('/');
+  if (exclude.some(fn => fn(mime))) {
+    if (!types[prefix].length) {
+      constants.delete(pre);
+      delete types[prefix];
+    }
+    continue;
+  }
+  let drop = false;
+  let i = types[prefix].indexOf(mime);
+  if (i < 0) {
+    drop = true;
+    i = types[prefix].push(mime) - 1;
+  }
+  for (const value of extensions) {
+    if (value in values) {
+      if (drop)
+        types[prefix].pop();
+    }
+    else {
+      values[value] = [prefix, i];
+    }
+  }
+}
+
+for (const source of sources) {
+  if (!valid.has(source)) {
+    console.error(`Unknown db source ${source}`);
+    process.exit(1);
+  }
+}
+
+if (oneOff) {
+  if (oneOff in values) {
+    const [prefix, i] = values[oneOff];
+    process.stdout.write(`${prefix}/${types[prefix][i]}`);
+    process.exit(0);
+  }
+  else {
+    console.error(`Unknown extension ${oneOff}`);
+    process.exit(1);
+  }
+}
+
+const replacements = [...constants.values()].join('|');
+
+let content = '';
+
+for (const [key, value] of constants)
+  content += `const ${key} = ${stringify(value)};\n`;
+
+content += `
+const types = ${stringify(types, null, '  ').replace(
+  new RegExp(`"(${replacements})":`, 'g'),
+  (_, $1) => `[${findKey($1)}]:`,
+)};
+
+const extensions = ${stringify(values, null, '  ').replace(
+  new RegExp(`"(${replacements})",`, 'g'),
+  (_, $1) => `${findKey($1)},`,
+)};
+
+const noDot = ${noDot};
+
+export default new Proxy(
+  extensions,
+  {
+    has: ($, type) => $.hasOwnProperty(noDot(type)),
+    get: ($, type) => {
+      const value = $[noDot(type)];
+      return value && \`\${value[0]}/\${types[value[0]][value[1]]}\`;
+    },
+  }
+);
+`;
+
+process.stdout.write(content);
